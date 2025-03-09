1b2472ee09f416f7a639d44c9d7640e78936e089
avrjakhov <a.v.rjakhov@yandex.ru>
2025-03-09 22:49:49 +0500

    feat[02-syntax]: add task5

---
+++ 02-syntax/task5/index.js
@@ -0,0 +1,23 @@
+const question1 = "Какая самая маленькая страна мира по населению?";
+const question2 = "Какая самая большая страна мира по населению?";
+const question3 = "Какой самый большой материк на планете?";
+
+const answer1 = "Китай";
+const answer2 = "Евразия";
+const answer3 = "Ватикан";
+
+console.log(question1, 'Ответ:', answer3);
+console.log(question2, 'Ответ:', answer1);
+console.log(question3, 'Ответ:', answer2);
+
+/* Вах! Тремя разными способами можно. Неплохо. Запомним
+console.log(`У машины изменилась скорость: ${maxCarSpeed} км/ч`);
+console.log(question1, 'Ответ:', answer3);
+console.log("Name: " + myName)
+
+На снимке, что приложен к задаче в доках указана вообще любые данные.
+log(...data: any[]): void;
+
+Хм. Следовательно, в работе со строками познакомимся
+
+*/
