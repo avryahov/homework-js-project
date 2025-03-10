fa4f133ffee832195ded9c832c50541982c72ac8
avrjakhov <a.v.rjakhov@yandex.ru>
2025-03-11 00:11:23 +0500

    feat[03-data-types]: add task1

---
+++ 03-data-types/task1/data-types.js
@@ -0,0 +1,23 @@
+const age = 25;
+console.log('age:', age, typeof age);
+
+const name = "Alexander";
+console.log('name:', name, typeof name);
+
+const isStudent = true;
+console.log('isStudent:', isStudent, typeof isStudent);
+
+const nullValue = null;
+console.log('nullValue:', nullValue, typeof nullValue);
+
+const undefinedValue = undefined;
+console.log('undefinedValue:', undefinedValue, typeof undefinedValue);
+
+const developer = {firstName: name, languages: ["Java", "JavaScript", "C/C++", "Ruby"]};
+console.log('person:', developer, typeof developer);
+
+const bigNumber = 162048025329125n;
+console.log('bigNumber:', bigNumber, typeof bigNumber);
+
+const id = Symbol("identifier");
+console.log('id:', id, typeof id);
