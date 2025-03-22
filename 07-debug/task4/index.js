6720d72f628787f3950d762bb586117250ff648b
avrjakhov <a.v.rjakhov@yandex.ru>
2025-03-23 03:41:37 +0500

    feat[07-debug]: add task4 without bugs

--- 07-debug/task4/index.js
+++ 07-debug/task4/index.js
@@ -28,15 +28,13 @@ debugger;
 
 // Через цикл for
 for (let i = 0; i < numbers.length; i += 1) {
-    numbers[i] = numbers[i] ** 3;
-    sum += numbers[i];
+    sum += numbers[i] ** 3;
 }
 console.log(sum); // 1158411
 
 // Через цикл for of
 sum = 0;
 for (let num of numbers) {
-    num = num ** 3;
-    sum += num;
+    sum += num ** 3;
 }
 console.log(sum); // 1003904306910622800
