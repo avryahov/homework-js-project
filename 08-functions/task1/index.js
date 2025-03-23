de665e256b69eced01949a4fe65ed7bc871d5a32
avrjakhov <a.v.rjakhov@yandex.ru>
2025-03-23 18:59:26 +0500

    feat[08-functions]: add task1

---
+++ 08-functions/task1/index.js
@@ -0,0 +1,11 @@
+function getName1(name) {
+    return `Имя равно ${name}`;
+}
+const getName2 = function(name) {
+    return `Имя равно ${name}`;
+};
+const getName3 = (name) => `Имя равно ${name}`;
+
+console.log(getName1("Вася"));
+console.log(getName2("Петя"));
+console.log(getName3("Маша"));
