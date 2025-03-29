1a60919abf122ee67e5dccbea474406472360fa0
avrjakhov <a.v.rjakhov@yandex.ru>
2025-03-30 00:18:20 +0500

    feat[12-keyword-this]: add task2

---
+++ 12-keyword-this/task2/index.js
@@ -0,0 +1,25 @@
+const dog = {
+    name: 'Чарли',
+    type: 'Собака',
+    makeSound() {
+        return 'Гав-Гав';
+    }
+};
+
+const bird = {
+    name: 'Петя',
+    type: 'Воробей',
+    makeSound() {
+        return 'Чик-чирик';
+    }
+};
+
+function makeDomestic(isDomestic) {
+    const newObject = { ...this, isDomestic };
+    console.log(`${this.type} по имени ${this.name} говорит ${this.makeSound()}`);
+    return newObject;
+}
+
+makeDomestic.bind(dog, true)();
+makeDomestic.call(bird, false);
+makeDomestic.apply(bird, [false]);
