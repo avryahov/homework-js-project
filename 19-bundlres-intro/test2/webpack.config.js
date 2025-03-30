b6385c38467836ca860ec68c907afb6867f40380
avrjakhov <a.v.rjakhov@yandex.ru>
2025-03-30 19:21:13 +0500

    feat[19-bundlres-intro]: add server

--- 19-bundlres-intro/test2/webpack.config.js
+++ 19-bundlres-intro/test2/webpack.config.js
@@ -9,6 +9,11 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true
     },
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 8080,
+        open: true
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'index.html'),
