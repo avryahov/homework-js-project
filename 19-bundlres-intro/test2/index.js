2e714497802274a5245041dd0f958fb398810370
avrjakhov <a.v.rjakhov@yandex.ru>
2025-03-30 19:00:08 +0500

    feat[19-bundlres-intro]: add images

--- 19-bundlres-intro/test2/index.js
+++ 19-bundlres-intro/test2/index.js
@@ -1,4 +1,5 @@
 import "./index.css";
+import MY_IMAGE from './assets/NulpBMmMmNQ.jpg';
 
 function hello() {
     console.log("Hello world!");
@@ -7,3 +8,7 @@ function hello() {
 hello();
 
 [1, 2, 3].map(n => n + 1);
+
+const img = document.createElement("img");
+img.src = MY_IMAGE;
+document.body.append(img);
--- 19-bundlres-intro/test2/webpack.config.js
+++ 19-bundlres-intro/test2/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: "development",
@@ -31,6 +30,10 @@ module.exports = {
             {
                 test: /.css$/i,
                 use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
             }
         ]
     }
