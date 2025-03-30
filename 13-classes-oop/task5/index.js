e894b38e4f4cdbce31467ca5c675fd926bbe7dcc
avrjakhov <a.v.rjakhov@yandex.ru>
2025-03-30 17:47:18 +0500

    feat[13-classes-oop]: add test example for task5

--- 13-classes-oop/task5/index.js
+++ 13-classes-oop/task5/index.js
@@ -55,8 +55,21 @@ class SeniorDeveloper extends Developer {
         console.log("Senior-разработчик пишет код...")
     }
 }
+const juniorDeveloper = new JuniorDeveloper('Анастасия', 20);
+const middleDeveloper = new MiddleDeveloper('Игорь', 25);
+const seniorDeveloper = new SeniorDeveloper('Максим', 30);
 
+juniorDeveloper.code(); // Junior разработчик пишет код...
+middleDeveloper.code(); // Middle разработчик пишет код...
+seniorDeveloper.code(); // Senior разработчик пишет код...
 
-const developer = new Developer("Alex", 34, Position.SENIOR);
+seniorDeveloper.learnNewTechnologies('Docker');
 
-console.log(developer);
+console.log(juniorDeveloper.fullName, juniorDeveloper.age, juniorDeveloper.position, juniorDeveloper.technologies);
+// 'Анастасия' 20 'Junior' ['HTML', 'CSS', 'JavaScript']
+
+console.log(middleDeveloper.fullName, middleDeveloper.age, middleDeveloper.position, middleDeveloper.technologies);
+// 'Игорь' 25 'Middle' ['HTML', 'CSS', 'JavaScript', 'React']
+
+console.log(seniorDeveloper.fullName, seniorDeveloper.age, seniorDeveloper.position, seniorDeveloper.technologies);
+// 'Максим' 30 'Senior' ['HTML', 'CSS', 'JavaScript', 'React', 'NodeJS', 'Docker']
