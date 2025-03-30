d842dba72911ccfd1720c9a86726e0225c5f8c1f
avrjakhov <a.v.rjakhov@yandex.ru>
2025-03-30 15:50:08 +0500

    feat[13-classes-oop]: add update task1

--- 13-classes-oop/task1/index.js
+++ 13-classes-oop/task1/index.js
@@ -1,21 +1,24 @@
-function Student(name, age) {
-    this.name = name;
-    this.age = age;
-    this.technologies = [];
-    this.status = 'Junior';
+class Student {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.technologies = [];
+        this.status = 'Junior';
+    }
 
-    this.setTechnologies = function(technologies) {
+    setTechnologies(technologies) {
         this.technologies = [
             ...this.technologies,
             ...technologies,
         ];
     }
-    this.setNewStatus = function(newStatus) {
+
+    setNewStatus(newStatus) {
         this.status = newStatus;
     }
 }
 
-const student = new Student ('Maxim', 20);
-student.setTechnologies([ 'HTML', 'CSS', 'JavaScript' ]);
+const student = new Student('Maxim', 20);
+student.setTechnologies(['HTML', 'CSS', 'JavaScript']);
 student.setNewStatus('Middle');
 console.log(student);
