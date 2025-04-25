c702f5a35d92abcfed2cb28c81a9f1ca77756bf3
avrjakhov <a.v.rjakhov@yandex.ru>
2025-04-25 18:31:11 +0500

    feat[14-working-with-dom-1]: modify task2 by example with style

--- 14-working-with-dom-1/task2/index.js
+++ 14-working-with-dom-1/task2/index.js
@@ -1,54 +1,55 @@
 const tasks = [
     {
-        id: '1138465078061',
+        id: "1138465078061",
         completed: false,
-        text: 'Посмотреть новый урок по JavaScript',
+        text: "Посмотреть новый урок по JavaScript",
     },
     {
-        id: '1138465078062',
+        id: "1138465078062",
         completed: false,
-        text: 'Выполнить тест после урока',
+        text: "Выполнить тест после урока",
     },
     {
-        id: '1138465078063',
+        id: "1138465078063",
         completed: false,
-        text: 'Выполнить ДЗ после урока',
+        text: "Выполнить ДЗ после урока",
     },
 ];
 
-function createTaskElement(task) {
-    const taskItem = document.createElement('div');
-    taskItem.className = 'task-item';
-    taskItem.setAttribute('data-task-id', task.id);
+function createTaskElement(taskId, taskText) {
+    const taskItem = document.createElement("div");
+    taskItem.className = "task-item";
+    taskItem.dataset.taskId = taskId;
 
-    const mainContainer = document.createElement('div');
-    mainContainer.className = 'task-item__main-container';
+    const mainContainer = document.createElement("div");
+    mainContainer.className = "task-item__main-container";
 
-    const mainContent = document.createElement('div');
-    mainContent.className = 'task-item__main-content';
+    const mainContent = document.createElement("div");
+    mainContent.className = "task-item__main-content";
 
-    const checkboxForm = document.createElement('form');
-    checkboxForm.className = 'checkbox-form';
+    const checkboxForm = document.createElement("form");
+    checkboxForm.className = "checkbox-form";
 
-    const checkboxInput = document.createElement('input');
-    checkboxInput.className = 'checkbox-form__checkbox';
-    checkboxInput.type = 'checkbox';
-    checkboxInput.id = `task-${task.id}`;
+    const checkboxInput = document.createElement("input");
+    checkboxInput.className = "checkbox-form__checkbox";
+    checkboxInput.type = "checkbox";
+    const inputId = `task-${taskId}`;
+    checkboxInput.id = inputId;
 
-    const checkboxLabel = document.createElement('label');
-    checkboxLabel.htmlFor = `task-${task.id}`;
+    const checkboxLabel = document.createElement("label");
+    checkboxLabel.htmlFor = inputId;
 
     checkboxForm.append(checkboxInput, checkboxLabel);
 
-    const taskText = document.createElement('span');
-    taskText.className = 'task-item__text';
-    taskText.textContent = task.text;
+    const taskItemText = document.createElement("span");
+    taskItemText.className = "task-item__text";
+    taskItemText.textContent = taskText;
 
-    mainContent.append(checkboxForm, taskText);
+    mainContent.append(checkboxForm, taskItemText);
 
-    const deleteButton = document.createElement('button');
-    deleteButton.className = 'task-item__delete-button default-button delete-button';
-    deleteButton.textContent = 'Удалить';
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "task-item__delete-button default-button delete-button";
+    deleteButton.textContent = "Удалить";
 
     mainContainer.append(mainContent, deleteButton);
     taskItem.append(mainContainer);
@@ -56,9 +57,9 @@ function createTaskElement(task) {
     return taskItem;
 }
 
-const tasksList = document.querySelector('.tasks-list');
+const tasksList = document.querySelector(".tasks-list");
 
 tasks.forEach(task => {
-    const taskElement = createTaskElement(task);
+    const taskElement = createTaskElement(task.id, task.text);
     tasksList.append(taskElement);
 });
