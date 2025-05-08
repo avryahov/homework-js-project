317aff07fab6aa2b833eaf6fecba56956e9f130f
avrjakhov <a.v.rjakhov@yandex.ru>
2025-05-08 20:18:38 +0500

    feat[16-async-promise-fetch]: add task2 with bug

--- 16-async-promise-fetch/task2/index.js
+++ 16-async-promise-fetch/task2/index.js
@@ -1,23 +1,46 @@
 const loader = document.getElementById("loader");
 const dataContainer = document.getElementById("data-container");
 
-loader.hidden = false;
 
 const LINK_USERS = "https://jsonplaceholder.typicode.com/users";
 
-fetch(LINK_USERS).then((response) => response.json())
-    .then(users => {
-        dataContainer.innerHTML = "";
-        users.forEach(user => {
-            const li = document.createElement("li");
-            const link = document.createElement("a");
-            link.href = "#";
-            link.textContent = user.name;
+const createUserElement = (text) => {
+    const userElement = document.createElement("li");
+    const userAnchorElement = document.createElement("a");
+    userAnchorElement.href = "#";
+    userAnchorElement.textContent = text;
+    userElement.append(userAnchorElement);
+    return userElement;
+};
 
-            li.appendChild(link);
-            dataContainer.appendChild(li);
-        });
-    }).catch(error => {
-    console.error("Не удалось загрузить данные:", error);
-})
-    .finally(loader.hidden = true)
+const getUsersByIds = (userIds) => {
+    loader.hidden = false;
+
+    const requests = userIds.map(id => {
+        const input = `${LINK_USERS}/${id}`;
+        return fetch(input)
+    });
+
+    Promise.all(requests)
+        .then(responses => {
+            return Promise.all(
+                responses
+                    .filter((response) => response.ok)
+                    .map((response) => response.json())
+            );
+        })
+        .then(users => {
+            dataContainer.innerHTML = "";
+
+            users.forEach(user => {
+                const userElement = createUserElement(user.name);
+                dataContainer.append(userElement);
+            });
+        })
+        .catch(error => {
+            console.error("Ошибка при получении пользователей:", error);
+        })
+        .finally(loader.hidden = true)
+};
+
+getUsersByIds([5, 6, 2, 1]);
