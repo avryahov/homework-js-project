df5e6096376211f8d5c8c0a551ac26af419b5844
avrjakhov <a.v.rjakhov@yandex.ru>
2025-05-09 16:36:23 +0500

    feat[17-async-await]: add task1

---
+++ 17-async-await/task1/index.js
@@ -0,0 +1,24 @@
+const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
+let isLoading = false;
+
+const createNewPost = async () => {
+    isLoading = true;
+    try {
+        const response = await fetch(POSTS_URL, {
+            method: "POST"
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log("result", result);
+    } catch (error) {
+        console.log("error", error);
+    } finally {
+        isLoading = false;
+    }
+};
+
+createNewPost();
